fbd990628c3ea8eb212578c8b8423bf127d3c2a5
vito <genovese.work@gmail.com>
2021-01-25 21:20:11 +0100

    All implementations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const courses = require("./courses.json");
 
 app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("../public/index");
+});
+
+app.get("/cursos", (req, res) => {
+    res.locals.courses = courses;
+    res.render("../public/course");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
